84be433d19f2940648c7903fb937463cd6eb72d7
leehua012 <gohhua12@gmail.com>
2024-01-29 19:06:59 +0800

    added backend spring boot

---
+++ backend/src/main/java/com/nusiss/inventory/backend/AdvancedInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nusiss.inventory.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nusiss/inventory/backend/AdvancedInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nusiss.inventory.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
