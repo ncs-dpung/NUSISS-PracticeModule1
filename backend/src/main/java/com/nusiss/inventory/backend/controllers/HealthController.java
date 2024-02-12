b554558add30f986770da02b587b0e0f1a4236d5
Derek Pung <derekpung@outlook.com>
2024-02-12 17:39:48 +0800

    be with healthcheck & docker compose

---
+++ backend/src/main/java/com/nusiss/inventory/backend/controllers/HealthController.java
@@ -0,0 +1,18 @@
+package com.nusiss.inventory.backend.controllers;
+
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/healthcheck")
+@Api(value = "Health Check", tags = "HealthCheck Controller")
+public class HealthController {
+    @ApiOperation(value = "server health")
+    @GetMapping
+    public String getAllUsers() {
+        return "HEALTHY";
+    }
+}
