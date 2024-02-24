24accf1ccf9d70040853c774a494ad75e17d3baa
Louiy <liux0105@e.ntu.edu.sg>
2024-02-24 20:08:06 +0800

    Update inventory add,update function

--- frontend/src/app/inventory-management/inventory-management.component.ts
+++ frontend/src/app/inventory-management/inventory-management.component.ts
@@ -1,19 +1,62 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {NgClass} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-inventory-management',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    FormsModule
+  ],
   templateUrl: './inventory-management.component.html',
   styleUrl: './inventory-management.component.scss'
 })
 export class InventoryManagementComponent {
 
-  constructor(private router: Router) {} 
+  showModal: boolean = false;
+  newItem: any = {
+    name: '',
+    condition: 'new',
+    available: 0,
+    reserved: 0,
+    price: '',
+    modified: new Date().toISOString().split('T')[0]
+  }; // Replace with your item model
+  showUpdateModal = false;
+  selectedItem: any = {};
 
-  navigate(path: string): void {
-    this.router.navigate([path]); 
+
+  toggleModal() {
+    this.showModal = !this.showModal;
   }
 
+  onSubmit() {
+    // Here you'd handle adding the new item to your inventory list
+    console.log('New item:', this.newItem);
+    // Close the modal
+    this.showModal = false;
+    // Reset the new item or do whatever is needed post-submission
+    this.newItem = {};
+  }
+  constructor(private router: Router) {}
+
+  navigate(path: string): void {
+    this.router.navigate([path]);
+  }
+
+  toggleUpdateModal(show: boolean) {
+    this.showUpdateModal = show;
+    if (show) {
+      //this.selectedItem = {...item}; // Assuming `item` is the item to update
+    }
+
+  }
+
+  onUpdateItem() {
+    // Implement update logic here, such as making an HTTP request to update the item in the backend
+    console.log(this.selectedItem);
+    this.toggleUpdateModal(false); // Hide modal after update
+  }
 }
