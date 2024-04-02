cb5200c5ce223200e799a87a980bdaa9e5dbaf57
Louiy <liux0105@e.ntu.edu.sg>
2024-04-02 15:45:56 +0800

    updated service

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Customer } from './customer-management/customer.model'; 
+import { Customer } from '../customer-management/customer.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8080/user'; 
+  private apiUrl = 'http://localhost:8080/user';
 
 
 
